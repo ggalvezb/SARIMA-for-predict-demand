8701a075075d569a7f8d52515f676891645a7856
ggalvezb <32131902+ggalvezb@users.noreply.github.com>
2019-11-21 17:35:47 -0300

    Update Sarima.py

--- Sarima.py
+++ Sarima.py
@@ -46,11 +46,16 @@ decomposition = sm.tsa.seasonal_decompose(y, model='additive')
 fig = decomposition.plot()
 plt.show()
 
-#SARIMA to time series forecasting
+####################                                   ########################
+#################### SARIMA to time series forecasting ########################
+####################                                   ########################
+
+##### ARIMA que mejor funciona ara eliminar la estacionalidad
 p = d = q = range(0, 2)
 pdq = list(itertools.product(p, d, q))
 seasonal_pdq = [(x[0], x[1], x[2], 53) for x in list(itertools.product(p, d, q))]
 
+'''
 for param in pdq:
     for param_seasonal in seasonal_pdq:
         try:
@@ -59,7 +64,9 @@ for param in pdq:
             print('ARIMA{}x{}12 - AIC:{}'.format(param,param_seasonal,results.aic))
         except: 
             continue
+'''
 
+#Calculo de SARIMAX
 mod = sm.tsa.statespace.SARIMAX(y,
                                 order=(0, 0, 1),
                                 seasonal_order=(1, 1, 0, 53),
@@ -72,6 +79,7 @@ results.plot_diagnostics(figsize=(10, 8))
 plt.show()
 plt.savefig('books_read.png')
 
+###REvision con datos reales
 pred = results.get_prediction(start=pd.to_datetime('2019-03-31'), dynamic=False)
 pred_ci = pred.conf_int()
 ax = y['2018':].plot(label='observed')
@@ -84,12 +92,27 @@ ax.set_ylabel('Retail_sold')
 plt.legend()
 plt.show()
 
+#Calculo de MSE y RMSE
 y_forecasted = pred.predicted_mean
-y_truth = y['2019-03-31':]
-mse = ((y_forecasted - y_truth) ** 2)
+y_truth = y['2019-03-31':]['valores']
+mse = ((y_forecasted - y_truth) ** 2).mean()
 print('The Mean Squared Error is {}'.format(round(mse, 2)))
 print('The Root Mean Squared Error is {}'.format(round(np.sqrt(mse), 2)))
 
-y_truth
-y_forecasted
-mse
+
+
+#Predicción de datos
+pred_uc = results.get_forecast(steps=53)
+pred_ci = pred_uc.conf_int()
+ax = y.plot(label='observed', figsize=(14, 4))
+pred_uc.predicted_mean.plot(ax=ax, label='Forecast')
+ax.fill_between(pred_ci.index,
+                pred_ci.iloc[:, 0],
+                pred_ci.iloc[:, 1], color='k', alpha=.25)
+ax.set_xlabel('Date')
+ax.set_ylabel('Sales')
+plt.legend()
+plt.show()
+
+forecast = pred_uc.predicted_mean
+forecast.head(50)
